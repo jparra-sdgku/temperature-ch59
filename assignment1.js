3390f101ce63ab4e748a76579af683886f66c3c7
jparra <jparra@sdgku.edu>
2025-06-16 19:28:25 -0700

    variables

---
+++ assignment1.js
@@ -0,0 +1,37 @@
+//1. Declare variables
+//using let
+let character = "Leo";
+let action = "danced";
+let object = "a giant rubber duck";
+let place = "the park";
+let time = "a sunny afternoon";
+
+//using CONST
+const occasion = "a surprise party";
+
+// 2.- Create a Story
+console.log("--- Funny Story ---");
+console.log(character + " was at " + place + " on " + time + "."  );
+console.log("Suddenly " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be " + occasion + " and everyone had a great time!");
+
+//3. Experiment with Different Values
+console.log("--- Different version of the Story ---");
+
+//change values to create a different story
+character = "alice";
+action = "Skipped";
+object = "a huge cake";
+place = "the beach";
+time = "a rainy evening";
+
+console.log("--- Funny Story ---");
+console.log(character + " was at " + place + " on " + time + "."  );
+console.log("Suddenly " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be " + occasion + " and everyone had an amazing time!");
+
+
+
+
+
+
---
+++ temperature-project/script.js
@@ -0,0 +1,30 @@
+console.log("script");
+
+//Variables 
+
+// string
+let myName = "Jose";
+let lastName = 'Parra';
+
+//numerical
+let age = 59;
+let mysize = 9;
+
+//Boolean
+let isStudent = false;
+
+console.log(myName + "\n" + lastName);
+console.log(age,mysize);
+
+//concatenation
+console.log();
+console.log("my name is "+ myName + " " + lastName);
+
+const pi = 3.1416;
+let area = 0;
+let radius = 3;
+
+area = pi * (radius * radius);
+console.log("The area is " + area.toFixed(2));
+
+
